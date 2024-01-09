9c04928e4cc822b7c2a83665352a139a0ab6b2cc
Tyler Wagner <tylerrwagner38@gmail.com>
2024-01-09 12:02:42 -0500

    Overcomplicated
    Recoded the entire project because it was well over complicated and I pulled the previous code from an existing project.

--- Messages/client.py
+++ Messages/client.py
@@ -1,26 +1,12 @@
 import socket
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 import os
 import random
-import string
 import time
 
-def derive_key(password: bytes) -> bytes:
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        salt=b"",  # No salt
-        iterations=100000,
-        length=32,
-        backend=default_backend()
-    )
-    key = kdf.derive(password)
-    return key
-
 def encrypt_message(key: bytes, iv: bytes, plaintext: bytes) -> bytes:
-    cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     encryptor = cipher.encryptor()
     ciphertext = encryptor.update(plaintext) + encryptor.finalize()
     return ciphertext
@@ -31,13 +17,11 @@ SERVER_PORT = 12345
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_HOST, SERVER_PORT))
 
-password = b"your_secret_password"
-key = derive_key(password)
-
-iv = os.urandom(16)
-
 while True:
-    message = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+    message = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(10))
+    key = os.urandom(32)
+    iv = os.urandom(16)
+
     encrypted_message = encrypt_message(key, iv, message.encode('utf-8'))
     client_socket.send(encrypted_message)
 
--- Messages/server.py
+++ Messages/server.py
@@ -1,21 +1,10 @@
 import socket
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 import os
 
-def derive_key(password: bytes, salt: bytes) -> bytes:
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        salt=salt,
-        iterations=100000,
-        length=32,
-    )
-    key = kdf.derive(password)
-    return key
-
 def decrypt_message(key: bytes, iv: bytes, ciphertext: bytes) -> bytes:
-    cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     decrypted_message = decryptor.update(ciphertext) + decryptor.finalize()
     return decrypted_message
@@ -33,11 +22,8 @@ while True:
     client_socket, addr = server_socket.accept()
     print(f"Accepted connection from {addr}")
 
-    salt = os.urandom(16)
-    password = b"your_secret_password"
-    key = derive_key(password, salt)
-
-    iv = os.urandom(16)
+    key = os.urandom(32)  # 32 bytes for AES-256
+    iv = os.urandom(16)   # 16 bytes for AES block size
 
     while True:
         encrypted_message = client_socket.recv(1024)
@@ -45,6 +31,6 @@ while True:
             break
 
         decrypted_message = decrypt_message(key, iv, encrypted_message)
-        print(f"Received: {decrypted_message.decode('utf-8')}")
+        print(f"Received: {decrypted_message}")
 
     client_socket.close()
