61f7184517557af00f5d7189dd2693d40cb4c7f5
Tyler Wagner <tylerrwagner38@gmail.com>
2024-01-09 11:37:51 -0500

    Update client to remove salt

--- Messages/client.py
+++ Messages/client.py
@@ -1,17 +1,20 @@
 import socket
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import os
 import random
 import string
+import time
 
-def derive_key(password: bytes, salt: bytes) -> bytes:
+def derive_key(password: bytes) -> bytes:
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
-        salt=salt,
+        salt=b"",  # No salt
         iterations=100000,
         length=32,
+        backend=default_backend()
     )
     key = kdf.derive(password)
     return key
@@ -28,15 +31,16 @@ SERVER_PORT = 12345
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_HOST, SERVER_PORT))
 
-salt = client_socket.recv(16)
 password = b"your_secret_password"
-key = derive_key(password, salt)
+key = derive_key(password)
 
 iv = os.urandom(16)
 
 while True:
-    # Generate a random message of length 10
     message = ''.join(random.choice(string.ascii_letters) for _ in range(10))
-
     encrypted_message = encrypt_message(key, iv, message.encode('utf-8'))
     client_socket.send(encrypted_message)
+
+    print(f"Sent: {message}")
+
+    time.sleep(1)
